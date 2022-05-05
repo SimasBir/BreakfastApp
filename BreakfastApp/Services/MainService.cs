e80f2a8ca9f25f3f0395e2b50b8df96dd5669f37
SimonasB <93397193+SimasBir@users.noreply.github.com>
2022-05-05 21:28:40 +0300

    Switched to basic multithread

--- BreakfastApp/Services/MainService.cs
+++ BreakfastApp/Services/MainService.cs
@@ -1,6 +1,7 @@
 ﻿using BreakfastApp.Helpers;
 using BreakfastApp.Models.Food;
 using BreakfastApp.Models.Appliances;
+using System.Threading;
 
 namespace BreakfastApp.Services
 {
@@ -14,19 +15,25 @@ namespace BreakfastApp.Services
             var stopwatch = System.Diagnostics.Stopwatch.StartNew();
             Console.WriteLine("~~~~~~");
 
+            Task thread1 = Task.Factory.StartNew(() => Cooking(eggAmount, baconAmount));
+            Task thread2 = Task.Factory.StartNew(() => Coffee.Make());
+            Task thread3 = Task.Factory.StartNew(() => Toast.Make());
+
+            Task.WaitAll(thread1, thread2, thread3);
+            Console.WriteLine("Breakfast is ready!");
+            stopwatch.Stop();
+
+            Console.WriteLine($"Making breakfast took {Math.Round(stopwatch.Elapsed.TotalSeconds, 2)} seconds");
+        }
+
+        private static void Cooking(int eggAmount, int baconAmount)
+        {
             if (eggAmount + baconAmount > 0)
             {
                 Stove.HeatUp();
                 Egg.Make(eggAmount);
                 Bacon.Make(baconAmount);
             }
-            Coffee.Make();
-            Toast.Make();
-
-            Console.WriteLine("Breakfast is ready!");
-            stopwatch.Stop();
-
-            Console.WriteLine($"Making breakfast took {Math.Round(stopwatch.Elapsed.TotalSeconds,2)} seconds");
         }
     }
 }
